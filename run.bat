9bf54eb0fb28bcf52898372b0633c89855defa50
Agostinho Ramos <agostinhopina095@gmail.com>
2020-09-02 01:35:21 +0100

    change .bat file dir

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+
+start sass --watch node_modules\font-awesome\scss\compiled_main.scss:static\css\font-awesome\main.css
+start sass --watch node_modules\bootstrap\scss\compiled_main.scss:static\css\bootstrap\main.css
