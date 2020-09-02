b25d0ed5726e975357181cc7fa0c7d91180ab126
Agostinho Ramos <agostinhopina095@gmail.com>
2020-09-02 01:54:54 +0100

    Call some dependencies

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
 
+start python manage.py runserver
 start sass --watch node_modules\font-awesome\scss\compiled_main.scss:static\css\font-awesome\main.css
 start sass --watch node_modules\bootstrap\scss\compiled_main.scss:static\css\bootstrap\main.css
