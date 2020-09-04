99baaf6f402302eb1774939571ecec59c9a9df2f
Agostinho Ramos <agostinhopina095@gmail.com>
2020-09-05 00:34:15 +0100

    Request method 'POST'

--- core/views.py
+++ core/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+#from .models import *
+import time
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+@csrf_exempt
+def post(request):
+    URL = request.POST["url"]
+    TYPE = request.POST["type"]
+    time.sleep(5)
+    RESPONSE = URL
+    return render(request, "response.html", {"RESPONSE" : RESPONSE})
--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,76 @@
 (function(){
-    const analyseBTN = document.querySelector('.main-action input[type="button"]');
+
+    const analyseBTN = document.querySelector('.main-action button[type="button"]');
     const analyseTEXT = document.querySelector('.main-action input[type="text"]');
     const resultContainer = document.querySelector('.result-container');
     const loadingDIV = resultContainer.querySelector('.spinner-grow');
-    const container = resultContainer.querySelector('.container');
+    const container = resultContainer.querySelector('.default.container');
+    const remoteContainer = resultContainer.querySelector('.remote.container');
+    const messageSYS = resultContainer.querySelector('.alert');
 
-    analyseBTN.addEventListener("click", function(){
+    startLoading = function(){
         resultContainer.classList.add('text-center');
         container.classList.add('d-none');
         loadingDIV.classList.remove("d-none");
-    }, false);
+        remoteContainer.classList.add('d-none');
+    }
 
     stopLoading = function(){
         resultContainer.classList.remove('text-center');
-        container.classList.remove('d-none');
         loadingDIV.classList.add("d-none");
     }
+
+    showMSG = function(message, type='alert-danger'){
+        messageSYS.classList.remove('d-none');
+        messageSYS.classList.add(type);
+        messageSYS.innerHTML = message;
+    }
+
+    hideMSG = function(){
+        messageSYS.setAttribute("class", "alert");
+        messageSYS.classList.add('d-none');
+        messageSYS.innerHTML = '';
+    }
+
+    request = function(url, params, method){
+        var http = new XMLHttpRequest();
+        http.open(method, url, true);
+        http.setRequestHeader('Content-type', 
+        'application/x-www-form-urlencoded');
+        http.onreadystatechange = function() {
+            if(http.readyState == 4 
+            && http.status == 200) {
+                resp = http.responseText;
+                if(resp.length > 1){
+                    html = resp;
+                    remoteContainer.innerHTML = html;
+                    remoteContainer.classList.remove('d-none');
+                }else{
+                    showMSG('Something went wrong! :(');
+                    container.classList.remove('d-none');
+                }
+                stopLoading();
+            }
+        }
+        http.send(params);
+    }
+
+    analyseBTN.addEventListener("click", function(){
+        // Clear..
+        hideMSG();
+        
+        var url = analyseTEXT.value;
+        if( url.length > 0 ){
+            startLoading();
+            request(
+                "/post", 
+                "url=" + url +
+                "&type=" + 'DESKTOP', 
+                "POST"
+            );
+        }
+        analyseBTN.blur();
+
+    }, false);
 
 })();
